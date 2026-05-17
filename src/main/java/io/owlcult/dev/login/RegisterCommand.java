664aac74e2af6af2b2b3f9f8153c19245b850082
MikhKuts <105879783+MikhKuts@users.noreply.github.com>
2026-05-17 19:37:09 +0300

    Added /register command

---
+++ src/main/java/io/owlcult/dev/login/RegisterCommand.java
@@ -0,0 +1,23 @@
+package io.owlcult.dev.login;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+
+public class RegisterCommand {
+	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(Commands.literal("register")
+        	.then(Commands.argument("password", StringArgumentType.string()))
+            .executes(
+                context -> {
+                    context.getSource().sendSystemMessage(Component.literal("Введите /register <password> для подтверждения пароля"));
+                    
+                    return 1;
+                }
+            )
+        );
+    }
+}
