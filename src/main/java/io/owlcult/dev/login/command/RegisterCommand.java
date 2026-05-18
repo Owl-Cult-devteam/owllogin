a79f40125c36eb09ecb8bc1e11d60de18373bce3
egevtech <egevtech@gmail.com>
2026-05-18 15:22:33 +0300

    Formatted

--- src/main/java/io/owlcult/dev/login/command/RegisterCommand.java
+++ src/main/java/io/owlcult/dev/login/command/RegisterCommand.java
@@ -2,22 +2,31 @@ package io.owlcult.dev.login.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
-
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 
 public class RegisterCommand {
-	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(Commands.literal("register")
-        	.then(Commands.argument("password", StringArgumentType.string()))
-            .executes(
-                context -> {
-                    context.getSource().sendSystemMessage(Component.literal("Введите /register <password> для подтверждения пароля"));
-                    
+
+    public static void register(
+        CommandDispatcher<CommandSourceStack> dispatcher
+    ) {
+        dispatcher.register(
+            Commands.literal("register")
+                .then(
+                    Commands.argument("password", StringArgumentType.string())
+                )
+                .executes(context -> {
+                    context
+                        .getSource()
+                        .sendSystemMessage(
+                            Component.literal(
+                                "Введите /register <password> для подтверждения пароля"
+                            )
+                        );
+
                     return 1;
-                }
-            )
+                })
         );
     }
 }
