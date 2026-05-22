236a6e5b3591d5ac44a40a4e3ff3e6c5857c1e86
egevtech <egevtech@gmail.com>
2026-05-22 15:55:28 +0300

    Add change_password command

---
+++ src/main/java/io/owlcult/dev/login/command/ChangePasswordCommand.kt
@@ -0,0 +1,59 @@
+package io.owlcult.dev.login.command
+
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.StringArgumentType
+import io.owlcult.dev.login.AuthController
+import io.owlcult.dev.login.AuthState
+import io.owlcult.dev.login.DatabaseManager
+import io.owlcult.dev.login.model.Player
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.Commands
+import net.minecraft.network.chat.Component
+
+class ChangePasswordCommand {
+    public fun register(
+        dispatcher: CommandDispatcher<CommandSourceStack>
+    ) {
+        dispatcher.register(
+            Commands.literal("change_password").then(
+                Commands.argument("new_password", StringArgumentType.string())
+                    .executes {
+                        context ->
+                        if (context.source.player == null)
+                            return@executes 0;
+
+                        val player = context.source.player!!
+
+                        when (AuthController.get_player_state(player.scoreboardName)){
+                            AuthState.NOT_REGISTERED -> {
+                                context.source.sendFailure(Component.literal("You're not registered"))
+
+                                return@executes 0;
+                            }
+
+                            AuthState.NOT_LOGGED_IN -> {
+                                context.source.sendFailure(Component.literal("Login first"))
+
+                                return@executes 0;
+                            }
+
+                            else -> {}
+                        }
+
+                        val dbman = DatabaseManager()
+
+                        val pmod = Player()
+
+                        pmod.nickname = player.scoreboardName
+                        pmod.password_hash = Player.hashPassword(context.getArgument("password", String::class.java))
+
+                        dbman.change(pmod)
+
+                        context.source.sendSystemMessage(Component.literal("Password changed!"))
+
+                        return@executes 1;
+                    }
+            )
+        )
+    }
+}
